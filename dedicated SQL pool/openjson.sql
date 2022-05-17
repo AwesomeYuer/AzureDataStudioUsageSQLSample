6341830a4591c40132f9f92fbf60a193bc62f5a4
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2022-05-17 21:26:50 +0800

    1

---
+++ dedicated SQL pool/openjson.sql
@@ -0,0 +1,15 @@
+create proc zsp_openjson
+as
+
+DECLARE @json NVARCHAR(2048) = N'{
+   "String_value": "John",
+   "DoublePrecisionFloatingPoint_value": 45,
+   "DoublePrecisionFloatingPoint_value": 2.3456,
+   "BooleanTrue_value": true,
+   "BooleanFalse_value": false,
+   "Null_value": null,
+   "Array_value": ["a","r","r","a","y"],
+   "Object_value": {"obj":"ect"}
+}';
+
+SELECT * FROM OpenJson(@json);
